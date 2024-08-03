7dcc5ee0c851e337d2d89b7c0662c775c8ad767c
jhwook <jhwooook@naver.com>
2024-08-03 21:18:14 +0900

    N14

--- Main.java
+++ Main.java
@@ -2,8 +2,8 @@ package src.main.java.calculator;
 
 public class Main {
     public static void main(String[] args) {
-        N67 sol = new N67();
-        System.out.println(sol.solution("aukks", "wbqd", 5));
+        N14 sol = new N14();
+        System.out.println(sol.solution(16));
 
     }
 }
---
+++ N14.java
@@ -0,0 +1,15 @@
+package src.main.java.calculator;
+
+public class N14 {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i = 1; i * i <= n; i++) {
+            if(n % i == 0) {
+                answer += i;
+                if(n != i * i) answer += n / i;
+            }
+        }
+        return answer;
+    }
+}
+//1 2 4 8 16
