3bb9802dd7e22678db5945812a4a2ac8c0fd8259
jhwook <jhwooook@naver.com>
2024-08-12 10:26:17 +0900

    N72

--- Main.java
+++ Main.java
@@ -1,14 +1,13 @@
 package src.main.java.calculator;
 
-import src.main.java.calculator.problems.N72;
-import src.main.java.calculator.problems.N72_hashMap;
+import src.main.java.calculator.problems.N73;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        N72_hashMap sol = new N72_hashMap();
-        System.out.println(Arrays.toString(sol.solution(new String[]{"mumu", "soe", "poe", "kai", "mine"}, new String[]{"kai", "kai", "mine", "mine"})));
+        N73 sol = new N73();
+        System.out.println(Arrays.toString(sol.solution(new String[]{"OSO","OOO","OXO","OOO"}, new String[]{"E 2","S 3","W 1"})));
 
     }
 }
