09261c1a063bb22ad25d9992c78df11849111cd8
jhwook <jhwooook@naver.com>
2024-08-20 10:50:39 +0900

    N74 신고자 알림

--- Main.java
+++ Main.java
@@ -3,13 +3,15 @@ package src.main.java.calculator;
 import src.main.java.calculator.problems.N29;
 import src.main.java.calculator.problems.N69;
 import src.main.java.calculator.problems.N73;
+import src.main.java.calculator.problems.N74;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        N69 sol = new N69();
-        System.out.println((sol.solution(new String[]{"TR", "RT", "TR"}, new int[]{7, 1, 3})));
+        N74 sol = new N74();
+        System.out.println(
+                (Arrays.toString(sol.solution(new String[]{"muzi", "frodo", "apeach", "neo"}, new String[]{"muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi", "apeach muzi"}, 2))));
 
     }
 }
--- problems/N74.java
+++ problems/N74.java
@@ -1,8 +1,51 @@
 package src.main.java.calculator.problems;
 
+import java.util.*;
+
 public class N74 {
     public int[] solution(String[] id_list, String[] report, int k) {
-        int[] answer = {};
+        int[] answer = new int[id_list.length];
+
+        Map<String,ArrayList<String>> reportMap = new HashMap<>();
+        Map<String, Integer> reportedMap = new HashMap<>();
+        ArrayList<String> suspendedUser = new ArrayList<>();
+        Map<String, Integer> reportResultMap = new HashMap<>();
+
+        HashSet<String> set = new HashSet<>(List.of(report));
+
+        String[] uniqueReport = set.toArray(new String[0]);
+
+        for(String id : id_list) {
+            reportMap.put(id, new ArrayList<>());
+            reportedMap.put(id, 0);
+        }
+
+        for(String r : uniqueReport) {
+            String reportUser = r.split(" ")[0];
+            String reportedUser = r.split(" ")[1];
+            reportMap.get(reportUser).add(reportedUser);
+            reportedMap.put(reportedUser, reportedMap.get(reportedUser) + 1);
+        }
+
+        for(Map.Entry<String, Integer> reportedEntry : reportedMap.entrySet()) {
+            if(reportedEntry.getValue() >= k) {
+                suspendedUser.add(reportedEntry.getKey());
+            }
+        }
+
+
+        for(Map.Entry<String, ArrayList<String>> reportEntry : reportMap.entrySet()) {
+            reportEntry.getValue().retainAll(suspendedUser);
+            reportResultMap.put(reportEntry.getKey(), reportEntry.getValue().size());
+
+        }
+
+        for(int i = 0; i < id_list.length; i++) {
+            answer[i] = reportResultMap.get(id_list[i]);
+        }
+
+
+
         return answer;
     }
 }
