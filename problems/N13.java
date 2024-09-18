73bf14f6151642a17a23956c42bbf7f6b7175b83
jhwook <jhwooook@naver.com>
2024-09-18 19:49:42 +0900

    N12

---
+++ problems/N13.java
@@ -0,0 +1,16 @@
+package src.main.java.calculator.problems;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class N13 {
+    public String[] solution(String[] players, String[] callings) {
+        String[] answer = {};
+        for(String calling : callings) {
+            int callingIndex = Arrays.asList(players).indexOf(calling);
+            Collections.swap(Arrays.asList(players), callingIndex, callingIndex - 1);
+        }
+        answer = players;
+        return answer;
+    }
+}
